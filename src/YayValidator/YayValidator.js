7e7e3a641a73bb352f7e9e316a7ddd9534863350
effi malach <effimalach@gmail.com>
2021-08-15 00:28:40 +0300

    small fix

--- src/YayValidator/YayValidator.js
+++ src/YayValidator/YayValidator.js
@@ -9,7 +9,7 @@ function YayValidator() {
 				When the input contains the value "yay",
 				change the input's background color to <span className="YayValidator__sample">green</span>:
 			</p>
-			<input onChange={(event)=> event.target.value.includes("yay") && setColor('green') } type="text" className="text-box"  style={{color}}/>
+			<input onChange={(event)=> event.target.value.includes("yay") && setColor('green') } type="text" className="text-box"  style={{background: color}}/>
 		</div>
 	);
 }
